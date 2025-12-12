3121486e792935d6778bde5340171c3d4faefff2
KHG <koo4934@gmail.com>
2025-12-12 09:31:23 +0900

    chore: init spring boot backend project

---
+++ backend/src/main/java/com/ssg9th2team/geharbang/GeharbangApplication.java
@@ -0,0 +1,13 @@
+package com.ssg9th2team.geharbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeharbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeharbangApplication.class, args);
+	}
+
+}
