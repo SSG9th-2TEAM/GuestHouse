7a72482dc1334ee18d0b595e3b9896bcec29ed7a
KHG <koo4934@gmail.com>
2025-12-15 16:00:47 +0900

    feat: dashboard basic set V2

---
+++ backend/src/main/java/com/ssg9th2team/geharbang/domain/dashboard/host/entity/HostSummaryRow.java
@@ -0,0 +1,11 @@
+package com.ssg9th2team.geharbang.domain.dashboard.host.entity;
+
+import lombok.Getter;
+
+@Getter
+public class HostSummaryRow {
+    private long expectedRevenue;
+    private int confirmedReservations;
+    private double avgRating;
+    private int operatingAccommodations;
+}
