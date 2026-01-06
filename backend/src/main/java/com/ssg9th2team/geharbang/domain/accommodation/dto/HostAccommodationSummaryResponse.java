1047950db9f248a061aea2b0cc3c3acf4f76a8bc
KHG <koo4934@gmail.com>
2026-01-06 15:07:11 +0900

    feat: host 자사, 소셜 공통화 작업, 개선

--- backend/src/main/java/com/ssg9th2team/geharbang/domain/accommodation/dto/HostAccommodationSummaryResponse.java
+++ backend/src/main/java/com/ssg9th2team/geharbang/domain/accommodation/dto/HostAccommodationSummaryResponse.java
@@ -2,13 +2,20 @@ package com.ssg9th2team.geharbang.domain.accommodation.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Setter
 public class HostAccommodationSummaryResponse {
     private Long accommodationsId;
     private String accommodationsName;
     private String status;
+    private String approvalStatus;
+    private Integer accommodationStatus;
+    private String rejectionReason;
     private String city;
     private String district;
     private String township;
